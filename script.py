3edd7dc3fb1afd02fc73d5c99cdc8cff37c45881
Poornesh <poornesh.thatturi@anywhere.co>
2025-07-30 12:52:52 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ ops_token = "ops_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIxMjM0NTY3ODkwIiw
 
 abc
 
-airtable_api_key = "key1234567890abcd7”
+ops_token = "ops_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpvc"
